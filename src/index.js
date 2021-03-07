a6fcc530e1c3c0ffa09dc0c7ca11870c6b43a420
Anuar Sabitovich <anuarkulmagambetov@gmail.com>
2021-03-07 18:48:49 +1100

    init: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let positive = Math.abs(n); // changing negative to positive values
+    let arr = Array.from(positive.toString()).map(Number); //making array of a digits
+    console.log(arr);
+    let revarr = arr.reverse(); // reversing array
+    console.log(revarr);
+    let joined = revarr.join(""); // joining array
+    return joined;
+};
+
+// let str = n.toString();
+// let first = str.split("");
+// let second = first.reverse("");
+// let third = second.join("");
+// let result = Number(third);
+// return result;
+
+// let first = str.split("");
+// let second = first.reverse("");
+// let third = second.join("");
+//   return third;
